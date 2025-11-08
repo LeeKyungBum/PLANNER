72e0f61c1c9181a1ab244bd0d0d626bb589df148
LeeKyungbum <lkb3311@gmail.com>
2025-11-08 10:10:43 +0900

    AI- 챗봇 api 구현

---
+++ back/planner/src/main/java/com/example/planner/ai/dto/AIResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.planner.ai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AIResponseDTO {
+    private String reply;
+}
